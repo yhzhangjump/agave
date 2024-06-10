95d7334f99c109df37de759e7caa389c4c7dcb11
Yunhao Zhang <yunzhang@jumptrading.com>
2024-06-10 21:10:23 +0000

    Print out the vote transactions received from TPU and gossip

--- core/src/cluster_info_vote_listener.rs
+++ core/src/cluster_info_vote_listener.rs
@@ -785,6 +785,7 @@ impl ClusterInfoVoteListener {
             .zip(repeat(/*is_gossip:*/ true))
             .chain(replayed_votes.into_iter().zip(repeat(/*is_gossip:*/ false)));
         for ((vote_pubkey, vote, _switch_proof, signature), is_gossip) in votes {
+            warn!("Yunhao: got gossip vote txn with pubkey={}, timestamp={:?}", vote_pubkey, vote.timestamp());
             Self::track_new_votes_and_notify_confirmations(
                 vote,
                 &vote_pubkey,
